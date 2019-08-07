7796913524151a72de6a197bbdd3e1a61f61c330
pavanidivya48 <pavanidivya48@gmail.com>
2019-08-07 07:38:12 +0530

    Initial Commit

---
+++ divyaSampleSDK/divyaSampleSDK.h
@@ -0,0 +1,19 @@
+//
+//  divyaSampleSDK.h
+//  divyaSampleSDK
+//
+//  Created by Ats on 07/08/19.
+//  Copyright © 2019 Ats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for divyaSampleSDK.
+FOUNDATION_EXPORT double divyaSampleSDKVersionNumber;
+
+//! Project version string for divyaSampleSDK.
+FOUNDATION_EXPORT const unsigned char divyaSampleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <divyaSampleSDK/PublicHeader.h>
+
+
